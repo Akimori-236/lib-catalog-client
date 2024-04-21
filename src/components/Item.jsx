78a73248bd4a4ea0a302a47b976990611799ab14
Wee Seng <wsng.try@gmail.com>
2024-04-22 01:35:42 +0800

    css

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 export default function Item(props) {
     return (
-            <div className='card col-sm-6 col-md-4 col-lg-3 m-3'>
-                {/* <td>{props.key}</td> */}
-                <h4>{props.item.title}
-                    <span className='badge text-bg-info'>{props.item.price}</span>
-                </h4>
-                {/* <td>{props.item.image}</td> */}
-                <td>{props.item.publisher}</td>
+        <div className='card col-sm-6 col-md-4 col-lg-3 m-3'>
+            {/* <td>{props.key}</td> */}
+            <h4>{props.item.title}
+                <div className='ms-auto'>
+                    <span className='badge text-bg-info '>{props.item.price}</span>
+                </div>
+            </h4>
+            {/* <td>{props.item.image}</td> */}
+            <td>{props.item.publisher}</td>
 
+            <div className='ms-auto mt-auto mb-2 me-0'>
                 <a href={props.item.link} className='btn btn-primary'>Shop</a>
             </div>
+        </div>
     );
 }
