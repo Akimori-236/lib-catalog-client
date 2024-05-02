5c67edd532480085f9d26be18b7a423dc990ea1f
Wee Seng <wsng.try@gmail.com>
2024-05-02 21:19:50 +0800

    added dark mode toggle

---
+++ src/components/DarkModeToggle.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+export default function ToggleTheme() {
+  const [isDarkTheme, setIsDarkTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.documentElement.setAttribute("data-bs-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-bs-theme", "light");
+    }
+    return savedTheme === "dark";
+  });
+
+  // save to browser on toggle
+  useEffect(() => {
+    localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+  }, [isDarkTheme]);
+
+  const toggleTheme = () => {
+    const rootElement = document.documentElement;
+    if (isDarkTheme) {
+      rootElement.setAttribute("data-bs-theme", "light");
+    } else {
+      rootElement.setAttribute("data-bs-theme", "dark");
+    }
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  return (
+    isDarkTheme ? <button onClick={toggleTheme} className="btn btn-light">
+      ☀️
+    </button> :
+      <button onClick={toggleTheme} className="btn btn-dark">
+        🌙
+      </button>
+  );
+};
